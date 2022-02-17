779a19e8316121b8e527f9191b967f372ce559cb
zeankundev <zeanfender11@gmail.com>
2022-02-17 08:27:53 +0700

    Initial commit

---
+++ media.js
@@ -0,0 +1,12 @@
+function vid1() {
+   var video = document.getElementById("mediavid"); video.src = ("medias/Morshu.mp4");
+}
+function vid2() {
+   var video = document.getElementById("mediavid"); video.src = ("medias/sigma.mp3");
+}
+function vid3() {
+   var video = document.getElementById("mediavid"); video.src = ("medias/bdp.mp4");
+}
+function vid4() {
+   var video = document.getElementById("mediavid"); video.src = ("medias/memeforest.wav");
+}

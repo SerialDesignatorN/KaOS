46d7fc4f4dda560f7a166216ba76d1fa3102155c
Bobuxstation <91587018+Bobuxstation@users.noreply.github.com>
2022-02-18 18:42:40 +0700

    Add files via upload

--- close.js
+++ close.js
@@ -35,7 +35,7 @@ function Closetestwindow5() {
   var x = document.getElementById("window5");
   if (x.style.display === "none") {
     x.style.display = "block";
-    var store = document.getElementById("store"); store.src = ("store.html");
+    var store = document.getElementById("store"); store.src = ("http://arkaria.42web.io/kaos12html/kaosstore/.html");
   } else {
     x.style.display = "none";
   }

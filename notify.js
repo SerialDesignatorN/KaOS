a56f234be0ac5458d6af32c2732ec0ae6baacc1b
zeankundev <zeanfender11@gmail.com>
2022-02-23 14:26:49 +0700

    fixed notification

--- notify.js
+++ notify.js
@@ -3,11 +3,12 @@ var li = document.createElement("li");
 li.appendChild(document.createTextNode("Welcome To Kaos 12!"));
 ul.appendChild(li);
 
-function notifications() {
-var ul = document.getElementById("list");
-var li = document.createElement("li");
-var br = document.createElement("br");
-li.appendChild(document.createTextNode(notification));
-ul.appendChild(br);
-ul.appendChild(li);
+function notifications(msg) {
+    var ul = document.getElementById("list");
+    var li = document.createElement("li");
+    var br = document.createElement("br");
+    li.appendChild(document.createTextNode(msg));
+    li.appendChild(br);
+    ul.appendChild(br);
+    ul.appendChild(li);
 }

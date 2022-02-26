d816eccd495d3adf60e95e7ad539bc6075f269fa
Bobuxstation <91587018+Bobuxstation@users.noreply.github.com>
2022-02-26 18:05:23 +0700

    Minimize windows update

--- close.js
+++ close.js
@@ -1,24 +1,30 @@
 function Closetestwindow() {
   var x = document.getElementById("window");
   if (x.style.display === "none") {
+    document.getElementById("minimizedfiles").style.display = "none";
     x.style.display = "block";
+    var files = document.getElementById("file"); files.src = ("medias/");
   } else {
     x.style.display = "none";
+    var files = document.getElementById("file"); files.src = ("");
   }
 }
 function Closetestwindow2() {
   var x = document.getElementById("window2");
   if (x.style.display === "none") {
+    document.getElementById("minimizedmeme").style.display = "none";
     x.style.display = "block";
     var mememaker = document.getElementById("mememaker"); mememaker.src = ("https://zeankundev.github.io/KaOS-Store/apps/MEMEMAKER.html");
   } else {
     x.style.display = "none";
+    var mememaker = document.getElementById("mememaker"); mememaker.src = ("");
   }
 }
 function Closetestwindow3() {
   var x = document.getElementById("window3");
   if (x.style.display === "none") {
     x.style.display = "block";
+    document.getElementById("minimizednotes").style.display = "none";
   } else {
     x.style.display = "none";
   }
@@ -27,8 +33,11 @@ function Closetestwindow4() {
   var x = document.getElementById("window4");
   if (x.style.display === "none") {
     x.style.display = "block";
+    var medias = document.getElementById("media"); media.src = ("MEDIA.html");
+    document.getElementById("minimizedmedia").style.display = "none";
   } else {
     x.style.display = "none";
+    var medias = document.getElementById("media"); media.src = ("");
   }
 }
 function Closetestwindow5() {
@@ -36,14 +45,17 @@ function Closetestwindow5() {
   if (x.style.display === "none") {
     x.style.display = "block";
     var store = document.getElementById("store"); store.src = ("https://zeankundev.github.io/KaOS-Store/");
+    document.getElementById("minimizedstore").style.display = "none";
   } else {
     x.style.display = "none";
+    var store = document.getElementById("store"); store.src = ("");
   }
 }
 function Closetestwindow6() {
   var x = document.getElementById("window6");
   if (x.style.display === "none") {
     x.style.display = "block";
+    document.getElementById("minimizedabout").style.display = "none";
   } else {
     x.style.display = "none";
   }
@@ -60,6 +72,7 @@ function Closetestwindow7() {
   var x = document.getElementById("window7");
   if (x.style.display === "none") {
     x.style.display = "block";
+    document.getElementById("minimizedsettings").style.display = "none";
   } else {
     x.style.display = "none";
   }
